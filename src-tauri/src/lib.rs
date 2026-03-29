9728350c6eb5e05c83cd4b25bc04009f682a3547
honononta <257093753+honononta@users.noreply.github.com>
2026-03-30 00:33:39 +0900

    シングルインスタンス化に変更

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -17,6 +17,9 @@ pub struct BackgroundActive(pub AtomicBool);
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_single_instance::init(|app, _args, _cwd| {
+            restore_window(app);
+        }))
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_shell::init())
         .setup(|app| {
